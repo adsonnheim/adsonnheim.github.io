595d63260833bb8fee97a44861b3be82b5664c79
srlcnsmr <adsonnheim@gmail.com>
2023-12-17 11:27:07 -0600

    First try at left-moving div
    Need to find out why border is disappearing as the div animates.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const projectDiv = document.querySelector(".projects");
 const resumeDiv = document.querySelector(".resume");
 const aboutDiv = document.querySelector(".about");
+const boxesDiv = document.querySelector(".boxes");
 
 const footer = document.querySelector(".footer");
+
+const testDiv = document.querySelector(".test");
 
 const divAnimationDown = [{opacity: "1", transform: "translateY(0vh)", offset: 0.0}, {opacity: "0", offset: 0.5},{opacity: "0", transform: "translateY(50vh)", offset: 1}];
 
@@ -10,9 +13,18 @@ const divAnimationUp = [{opacity: "1", transform: "translateY(0vh)", offset: 0.0
 
 const footerAnimation = [{opacity: "1"}, {opacity: "0", offset: 0.5}, {opacity: "0"}];
 
+const testAnimation = [{left: "-30vw"}, {left: "calc(50% - 12.5vw)"}];
+
 const timing = {
-  duration: 750,
+  duration: 500,
   iterations: 1,
+  fill: "forwards"
+};
+
+const slowTimer = {
+  duration: 5000,
+  iterations: 1,
+  fill: "forwards"
 };
 
 // TODO: Make left and right divs move away from center before descending.
@@ -24,6 +36,17 @@ function moveAll() {
   resumeDiv.animate(divAnimationUp, timing);
 
   footer.animate(footerAnimation, timing);
+
+  setTimeout(function () {
+    boxesDiv.remove();
+    resumeDiv.remove();
+    aboutDiv.remove();
+    projectDiv.remove();
+    footer.remove();
+    testDiv.animate(testAnimation, slowTimer);
+  }, 500);
+
+  document.body.appendChild(footer);
 }
 
 projectDiv.addEventListener("click", moveAll);
