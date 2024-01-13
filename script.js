574c9abf73b162ccb03768c31d5c28cf047cd239
Alan Sonnheim <150724577+adsonnheim@users.noreply.github.com>
2024-01-13 09:42:08 -0600

    Added a back button and starting to implement main content area
    Back button was added, although full functionality is not yet there.

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const projectDiv = document.querySelector(".projects");
 const resumeDiv = document.querySelector(".resume");
 const aboutDiv = document.querySelector(".about");
 const boxesDiv = document.querySelector(".boxes");
-
 const footer = document.querySelector(".footer");
+const back = document.querySelector(".back");
 
-const testDiv = document.querySelector(".test");
+const projectsSheet = document.querySelector(".projectsSheet");
+const resumeSheet = document.querySelector(".resumeSheet");
+const aboutSheet = document.querySelector(".aboutSheet");
 
 const divAnimationDown = [{opacity: "1", transform: "translateY(0vh)", offset: 0.0}, {opacity: "0", offset: 0.5},{opacity: "0", transform: "translateY(50vh)", offset: 1}];
 
@@ -13,22 +15,25 @@ const divAnimationUp = [{opacity: "1", transform: "translateY(0vh)", offset: 0.0
 
 const footerAnimation = [{opacity: "1"}, {opacity: "0", offset: 0.5}, {opacity: "0"}];
 
-const testAnimation = [{left: "-30vw"}, {left: "calc(50% - 12.5vw)"}];
+const sheetAnimation = [{"animation-timing-function:": "ease-in", left: "-30vw"}, {left: "calc(50% - 12.5vw)"}];
+
+const backAnimation = [{opacity: "0"}, {opacity: "1"}];
+
+const backAnimationDisappear = [{opacity: "1"}, {opacity: "0"}];
 
 const timing = {
   duration: 500,
   iterations: 1,
-  fill: "forwards"
+  fill: "forwards" 
 };
 
-const slowTimer = {
-  duration: 5000,
+const slowTiming = {
+  duration: 1000,
   iterations: 1,
   fill: "forwards"
 };
 
-// TODO: Make left and right divs move away from center before descending.
-function moveAll() {
+function moveCenterBoxes() {
   projectDiv.animate(divAnimationDown, timing);
 
   aboutDiv.animate(divAnimationDown, timing);
@@ -37,18 +42,56 @@ function moveAll() {
 
   footer.animate(footerAnimation, timing);
 
-  setTimeout(function () {
-    boxesDiv.remove();
-    resumeDiv.remove();
-    aboutDiv.remove();
-    projectDiv.remove();
-    footer.remove();
-    testDiv.animate(testAnimation, slowTimer);
-  }, 500);
+  back.animate(backAnimation, slowTiming);
 
-  document.body.appendChild(footer);
+  document.getElementById("backID").style.pointerEvents = "all";
 }
 
-projectDiv.addEventListener("click", moveAll);
-resumeDiv.addEventListener("click", moveAll);
-aboutDiv.addEventListener("click", moveAll);
+function removeCenterBoxes() {
+  projectDiv.remove();
+  aboutDiv.remove();
+  resumeDiv.remove();
+  footer.remove();
+}
+
+// TODO: Make left and right divs move away from center before descending.
+function moveProjects() {
+  moveCenterBoxes();
+
+  setTimeout(function () {
+    projectsSheet.animate(sheetAnimation, timing);
+
+    //removeCenterBoxes();
+  }, 450);
+}
+
+function moveResume() {
+  moveCenterBoxes();
+
+  setTimeout(function () {
+    resumeSheet.animate(sheetAnimation, timing);
+
+    //removeCenterBoxes();
+  }, 450);
+}
+
+function moveAbout() {
+  moveCenterBoxes();
+
+  setTimeout(function () {
+    aboutSheet.animate(sheetAnimation, timing);
+
+    //removeCenterBoxes();
+  }, 450);
+}
+
+function resetPage() {
+  back.animate(backAnimationDisappear, slowTiming);
+  document.getElementById("backID").style.pointerEvents = "none";
+}
+
+projectDiv.addEventListener("click", moveProjects);
+resumeDiv.addEventListener("click", moveResume);
+aboutDiv.addEventListener("click", moveAbout);
+
+back.addEventListener("click", resetPage);
